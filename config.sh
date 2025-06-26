76297363185dc7238e5ef718728d12659a2308f6
provoware <provoware.one@gmail.com>
2025-06-27 01:00:27 +0200

    Add config and React component for update script

---
+++ config.sh
@@ -0,0 +1,6 @@
+# Global configuration for scripts
+# Default installation directory for the frontend
+INSTALLDIR="$HOME/OneUserTool"
+
+# Version of the ModulesContainer component
+MODULE_VERSION="1.1"
